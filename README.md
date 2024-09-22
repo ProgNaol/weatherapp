36a0b72cb3e3b5ac283c66694c6d2803347d2880
ProgNaol <139908666+ProgNaol@users.noreply.github.com>
2024-09-22 07:52:44 +0300

    Add the weather app code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # weatherapp
  THis is a weather app made with ejs, css and js with api or real time data production
+
+ How to use 
+
+ 1 open the code in vs-code
+ 2 install the packages using npm i
+ 3 run the project using node app.js or if you have nodemon use nodmon app.js
+ 4 go too https://localhost:3000
+ 5 you can see a input place add any city you want and you will get
+    1 will it rain or not
+    2 the temprature
+    3 the Latitude
+    4 the longitude
+    5 the humidity
+
+
+Thank you for using the weather app,
+if you have any error using the weather app, email me at ndemisse2007@outlook.com
---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/', async (req, res) => {
+    const city = req.body.city;  
+    const apiKey = 'a95632f8fc85944acedfc23eb09ef234'; 
+    
+    try {
+        const geoResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        const { lat, lon } = geoResponse.data.coord;
+
+        const forecastResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+
+        const tomorrowForecast = forecastResponse.data.list[1];
+
+            const willRain = tomorrowForecast.weather.some(w => w.main.toLowerCase() === 'rain');
+            const temperature = tomorrowForecast.main.temp;
+            const clouds = tomorrowForecast.clouds.all;
+            const humidity = tomorrowForecast.main.humidity
+
+            res.render('index', { city, willRain, temperature, clouds, lat, lon, humidity });
+
+    } catch (error) {
+        res.render('error', { message: error.message });
+    }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
